e8672db0d39d34b4183cedbb82c74e79bf01ffda
Martín Mateos Sánchez <marmatsan.dev@gmail.com>
2023-10-27 21:05:18 +0200

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    // Custom Gradle plugins
+    includeBuild("./plugins")
+}
+
+plugins {
+    id("com.marmatsan.dependencies") apply true
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "time-matters"
+include(
+    ":app"
+)
+ 
